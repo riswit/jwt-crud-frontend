0620ecee174cf53b9a4cf87beff280dd5e3cd10b
Dino <dino.solano@riswit.it>
2019-03-31 16:56:47 +0200

    integrated AdminLTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppsettingsComponent } from './components/appsettings/appsettings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppfooterComponent,
+    AppmenuComponent,
+    AppsettingsComponent
   ],
   imports: [
     BrowserModule,
