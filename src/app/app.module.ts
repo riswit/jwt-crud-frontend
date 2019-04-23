5d176011e7333190483d991ac2365bd070f9252d
Dino <dino.solano@riswit.it>
2019-04-23 22:13:43 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { AppheaderComponent } from './components/appheader/appheader.component';
 import { AppfooterComponent } from './components/appfooter/appfooter.component';
 import { AppmenuComponent } from './components/appmenu/appmenu.component';
 import { AppsettingsComponent } from './components/appsettings/appsettings.component';
+
+import { LoginComponent } from './authentication/login/login.component';
+import { UserComponent } from './authentication/user/user.component';
+import { RegisterComponent } from './authentication/register/register.component';
+import { HomeComponent } from './authentication/home/home.component';
+import { AdminComponent } from './authentication/admin/admin.component';
+import { PmComponent } from './authentication/pm/pm.component';
+
+import { httpInterceptorProviders } from './authentication/auth/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -14,13 +25,21 @@ import { AppsettingsComponent } from './components/appsettings/appsettings.compo
     AppheaderComponent,
     AppfooterComponent,
     AppmenuComponent,
-    AppsettingsComponent
+    AppsettingsComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    UserComponent,
+    PmComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
