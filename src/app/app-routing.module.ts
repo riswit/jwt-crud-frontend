5d49e9b46bfcb2c5de9ddafd97059b9d245a9a34
Dino <dino.solano@riswit.it>
2019-04-24 23:33:54 +0200

    app model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+import { RouterModule, Routes } from '@angular/router';
+ 
+import { RegisterComponent } from './Authentication/register/register.component';
+import { LoginComponent } from './Authentication/login/login.component';
+import { HomeComponent } from './Authentication/home/home.component';
+import { UserComponent } from './Authentication/user/user.component';
+import { PmComponent } from './Authentication/pm/pm.component';
+import { AdminComponent } from './Authentication/admin/admin.component';
+ 
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'user',
+        component: UserComponent
+    },
+    {
+        path: 'pm',
+        component: PmComponent
+    },
+    {
+        path: 'admin',
+        component: AdminComponent
+    },
+    {
+        path: 'auth/login',
+        component: LoginComponent
+    },
+    {
+        path: 'signup',
+        component: RegisterComponent
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
+ 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
